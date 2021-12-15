23d319d39eb29ad068292ded19532617d3bf2fa4
nileshk147 <nileshk147@gmail.com>
2021-12-15 13:43:51 +0530

    message changes

--- schema/application-schema.js
+++ schema/application-schema.js
@@ -3,7 +3,7 @@ const schema = mongoose.Schema;
 
 const applicationSchema = new schema({
     application_name: {type:  String, required: true},
-    status: { type: Boolean, default: true},
+    status: { type: Boolean, default: false},
     outage_start_time: { type: Date, required: false},
     outage_end_time: { type: Date, required: false},
     recieved_from:{
